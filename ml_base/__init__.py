9c1f31db86b2cf3e627332c12f5225f2a2c9fcd3
schmidtbri <6666331+schmidtbri@users.noreply.github.com>
2021-01-29 00:50:02 -0500

    Adding release automation to project (#4)
    * adding release automation
    
    * adding release automation
    
    * adding release automation
    
    * adding release automation
    
    * adding release automation
    
    * testing code on pull request
    
    * testing code on pull request

--- ml_base/__init__.py
+++ ml_base/__init__.py
@@ -2,7 +2,7 @@
 
 from ml_base.ml_model import MLModel
 
-__version_info__ = ("0", "1", "1")
-__version__ = ".".join(__version_info__)
 
 __all__ = ["MLModel"]
+
+__version__ = "<version_placeholder>"
